0cc5769ca23a43ab017d82185fd026a8c5fd6490
lsn5963 <lsn5963@naver.com>
2022-08-17 14:26:09 +0900

    fetch board

---
+++ board/src/main/java/com/study/board/controller/BoardController.java
@@ -0,0 +1,37 @@
+package com.study.board.controller;
+
+import com.study.board.entity.Board;
+import com.study.board.service.BoardService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
+
+@Controller
+
+public class BoardController {
+    @Autowired
+    private BoardService boardService;
+
+    @GetMapping("/board/write") //어떤 url?
+    public String boardWriteForm() {
+
+        return "boardwrite";
+    }
+
+    @PostMapping("/board/writepro")
+    public String boardWritePro(Board board){
+        System.out.println(board.getContent());
+        System.out.println(board.getId());
+        boardService.write(board);
+        return "";
+    }
+    @GetMapping("/board/list")
+    public String boardList(Model model){
+        model.addAttribute("list", boardService.boardList());
+        return "boardlist";
+    }
+}
---
+++ board/src/main/java/com/study/board/entity/Board.java
@@ -0,0 +1,28 @@
+package com.study.board.entity;
+
+import lombok.*;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity //테이블을 의미한다.
+@Getter
+public class Board {
+    public Board(Integer id, String title, String content) {
+        this.id = id;
+        this.title = title;
+        this.content = content;
+    }
+
+
+    public Board(){
+
+    }
+    @Id //primary key를 의미
+    @GeneratedValue(strategy = GenerationType.IDENTITY) //mariadb에서 사용하는 것
+    private Integer id;
+    private String title;
+    private String content;
+}
