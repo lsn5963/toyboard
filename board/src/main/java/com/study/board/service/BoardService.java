9f36647cde543232a7bf244ab149436f2d889a35
lsn5963 <lsn5963@naver.com>
2022-09-05 19:16:55 +0900

    feat: 파일 업로드

--- board/src/main/java/com/study/board/service/BoardService.java
+++ board/src/main/java/com/study/board/service/BoardService.java
@@ -4,9 +4,11 @@ import com.study.board.entity.Board;
 import com.study.board.repository.BoardRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.ui.Model;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
 import java.util.List;
+import java.util.UUID;
 
 @Service
 public class BoardService {
@@ -15,20 +17,35 @@ public class BoardService {
     private BoardRepository boardRepository;
 
     //글작성
-    public void write(Board board){
+    public void write(Board board, MultipartFile file) throws Exception{
+
+        String projectPath = System.getProperty("user.dir") + "\\src\\main\\resources\\static\\files";
+
+        UUID uuid = UUID.randomUUID();
+
+        String fileName = uuid + "_" + file.getOriginalFilename();
+
+        File saveFile = new File(projectPath, fileName);
+
+        file.transferTo(saveFile);
+
+        board.setFilename(fileName);
+        board.setFilepath("/files/" + fileName);
+
         boardRepository.save(board);
     }
 
     //게시글 리스트 처리
-    public List<Board> boardList(){
+    public List<Board> boardList() {
         return boardRepository.findAll();
     }
 
     //특정 게시글 불러오기
-    public Board boardView(Integer id){
+    public Board boardView(Integer id) {
         return boardRepository.findById(id).get();
     }
-    public void BoardDelete(Integer id){
+
+    public void BoardDelete(Integer id) {
         boardRepository.deleteById(id);
     }
 }
