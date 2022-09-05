0d035c70fcb2b5ff08560464abf2102f89ae321b
lsn5963 <lsn5963@naver.com>
2022-09-05 20:13:25 +0900

    Revert "Revert "feat: 검색 기능""
    This reverts commit e3263e31c2cba536c0cf7d32031be2aee663e71a.

--- board/src/main/java/com/study/board/controller/BoardController.java
+++ board/src/main/java/com/study/board/controller/BoardController.java
@@ -40,8 +40,15 @@ public class BoardController {
 
 
     @GetMapping("/board/list")
-    public String boardList(Model model, @PageableDefault(page = 0, size = 10, sort = "id", direction = Sort.Direction.DESC) Pageable pageable){
-        Page<Board> list = boardService.boardList(pageable);
+    public String boardList(Model model, @PageableDefault(page = 0, size = 10, sort = "id", direction = Sort.Direction.DESC)
+    Pageable pageable, String searchKeyword){
+        Page<Board> list = null;
+        if (searchKeyword == null) {
+            list = boardService.boardList(pageable);
+        }else{
+            list = boardService.boardSearchList(searchKeyword, pageable);
+        }
+
         int nowPage = list.getPageable().getPageNumber() + 1;
         int startPage = Math.max(nowPage -4, 1);
         int endPage =   Math.min(nowPage +5, list.getTotalPages());
--- board/src/main/java/com/study/board/service/BoardService.java
+++ board/src/main/java/com/study/board/service/BoardService.java
@@ -46,6 +46,9 @@ public class BoardService {
         return boardRepository.findById(id).get();
     }
 
+    public Page<Board> boardSearchList(String searchKeyword, Pageable pageable){
+        return boardRepository.findByTitleContaining(searchKeyword, pageable);
+    }
     public void BoardDelete(Integer id) {
         boardRepository.deleteById(id);
     }
